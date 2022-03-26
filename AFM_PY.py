7c05897dba33488c04c54ee158b42f730b274786
PodorCN <zixiao@podor.org>
2022-03-26 11:35:10 -0400

    update indicators

--- AFM_PY.py
+++ AFM_PY.py
@@ -7,6 +7,8 @@ import random
 from sympy import false
 import pandas_techinal_indicators as ta #https://github.com/Crypto-toolbox/pandas-technical-indicators/blob/master/technical_indicators.py
 import pandas as pd
+
+import pandas_datareader as web
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, precision_score, confusion_matrix, recall_score, accuracy_score
 from sklearn.model_selection import train_test_split
@@ -25,6 +27,15 @@ del(aapl['Date'])
 del(aapl['Adj Close'])
 aapl.head()
 
+tickers = ["AMD", "ATVI", "BABA", "BIDU", "BILI","CEA","GME","GOOGL","HUYA","NVDA"]
+multpl_stocks = web.get_data_yahoo(tickers,
+start = "2018-11-01",
+end = "2020-03-31")
+
+indicators = ["MACD","OBV",'PROC',"Stochastic Oscillator"]
+stocks = ["APPL",]
+
+
 # Get Data
 
 # 各种calculator先define上
@@ -39,6 +50,100 @@ aapl.head()
 #   画图
 #   Metrics  
 
+def rate_of_change(df, n):
+    """
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    M = df['Close'].diff(n - 1)
+    N = df['Close'].shift(n - 1)
+    ROC = pd.Series(M / N, name='ROC_' + str(n))
+    df = df.join(ROC)
+    return df
+
+def on_balance_volume(df, n):
+    """Calculate On-Balance Volume for given data.
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    i = 0
+    OBV = [0]
+    while i < df.index[-1]:
+        if df.loc[i + 1, 'Close'] - df.loc[i, 'Close'] > 0:
+            OBV.append(df.loc[i + 1, 'Volume'])
+        if df.loc[i + 1, 'Close'] - df.loc[i, 'Close'] == 0:
+            OBV.append(0)
+        if df.loc[i + 1, 'Close'] - df.loc[i, 'Close'] < 0:
+            OBV.append(-df.loc[i + 1, 'Volume'])
+        i = i + 1
+    OBV = pd.Series(OBV)
+    OBV_ma = pd.Series(OBV.rolling(n, min_periods=n).mean(), name='OBV_' + str(n))
+    df = df.join(OBV_ma)
+    return df
+
+def stochastic_oscillator_k(df):
+    """Calculate stochastic oscillator %K for given data.
+    
+    :param df: pandas.DataFrame
+    :return: pandas.DataFrame
+    """
+    SOk = pd.Series((df['Close'] - df['Low']) / (df['High'] - df['Low']), name='SO%k')
+    df = df.join(SOk)
+    return df
+
+def macd(df, n_fast, n_slow):
+    """Calculate MACD, MACD Signal and MACD difference
+    
+    :param df: pandas.DataFrame
+    :param n_fast: 
+    :param n_slow: 
+    :return: pandas.DataFrame
+    """
+    EMAfast = pd.Series(df['Close'].ewm(span=n_fast, min_periods=n_slow).mean())
+    EMAslow = pd.Series(df['Close'].ewm(span=n_slow, min_periods=n_slow).mean())
+    MACD = pd.Series(EMAfast - EMAslow, name='MACD_' + str(n_fast) + '_' + str(n_slow))
+    MACDsign = pd.Series(MACD.ewm(span=9, min_periods=9).mean(), name='MACDsign_' + str(n_fast) + '_' + str(n_slow))
+    MACDdiff = pd.Series(MACD - MACDsign, name='MACDdiff_' + str(n_fast) + '_' + str(n_slow))
+    df = df.join(MACD)
+    df = df.join(MACDsign)
+    df = df.join(MACDdiff)
+    return df
+
+def relative_strength_index(df, n):
+    """Calculate Relative Strength Index(RSI) for given data.
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    i = 0
+    UpI = [0]
+    DoI = [0]
+    while i + 1 <= df.index[-1]:
+        UpMove = df.loc[i + 1, 'High'] - df.loc[i, 'High']
+        DoMove = df.loc[i, 'Low'] - df.loc[i + 1, 'Low']
+        if UpMove > DoMove and UpMove > 0:
+            UpD = UpMove
+        else:
+            UpD = 0
+        UpI.append(UpD)
+        if DoMove > UpMove and DoMove > 0:
+            DoD = DoMove
+        else:
+            DoD = 0
+        DoI.append(DoD)
+        i = i + 1
+    UpI = pd.Series(UpI)
+    DoI = pd.Series(DoI)
+    PosDI = pd.Series(UpI.ewm(span=n, min_periods=n).mean())
+    NegDI = pd.Series(DoI.ewm(span=n, min_periods=n).mean())
+    RSI = pd.Series(PosDI / (PosDI + NegDI), name='RSI_' + str(n))
+    df = df.join(RSI)
+    return df
 
 # Data Smoothing Processor---------------------------------------------------
 # Not Done, need look into it
@@ -47,7 +152,11 @@ def get_exp_preprocessing(df, alpha=0.9):
     return edata
 # Data Smoothing Processor---------------------------------------------------
 
-
+def hyptertune(estimator, X_train, y_train, param_grid, X_test):
+    grid_search = GridSearchCV(estimator = estimator, param_grid = param_grid, njobs = -1, verbose = 2)
+    grid_search.fit(X_train, y_train)
+    pred = grid_search.predict(X_test)
+    return pred
 # Indicator Calculator-------------------------------------------------------
 
 class calculator:
@@ -90,6 +199,12 @@ class stock:
 saapl = get_exp_preprocessing(aapl)
 saapl.head() #saapl stands for smoothed aapl
 
+
+#for stock in stocks:
+
+
+
+
 def feature_extraction(data):
     for x in [5, 14, 26, 44, 66]:
         data = ta.relative_strength_index(data, n=x)
@@ -131,11 +246,7 @@ def prepare_data(df, horizon):
     del(data['Close'])
     return data.dropna()
 
-def hyptertune(estimator, X_train, y_train, param_grid, X_test):
-    grid_search = GridSearchCV(estimator = estimator, param_grid = param_grid, njobs = -1, verbose = 2)
-    grid_search.fit(X_train, y_train)
-    pred = grid_search.predict(X_test)
-    return pred
+
 
 
 data = prepare_data(saapl, 10)
